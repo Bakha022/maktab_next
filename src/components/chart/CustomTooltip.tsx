04101cf42eea68a9ce1fa9b1229abd9a96bc81a0
Bakha022 <abduqahhorovbahodir536@gmail.com>
2025-03-14 15:07:44 +0500

    tollib undifined paylod change

--- src/components/chart/CustomTooltip.tsx
+++ src/components/chart/CustomTooltip.tsx
@@ -1,26 +1,26 @@
-import { TooltipProps } from 'recharts'
+import { TooltipProps } from 'recharts';
 
 const CustomTooltip: React.FC<TooltipProps<number, string>> = ({
-	active,
-	payload,
+  active,
+  payload,
 }) => {
-	if (active && payload && payload.length) {
-		return (
-			<div
-				style={{
-					background: '#00359E',
-					padding: '8px 12px',
-					borderRadius: '8px',
-					boxShadow: '0px 4px 10px rgba(0,0,0,0.1)',
-					textAlign: 'center',
-				}}
-			>
-				<p className="text-[12px] font-inter font-semibold leading-4.5 text-white">
-					{payload[0].value.toLocaleString()} foydalanuvchi
-				</p>
-			</div>
-		);
-	}
-	return null;
+  if (active && payload && payload.length && payload[0].value !== undefined) {
+    return (
+      <div
+        style={{
+          background: '#00359E',
+          padding: '8px 12px',
+          borderRadius: '8px',
+          boxShadow: '0px 4px 10px rgba(0,0,0,0.1)',
+          textAlign: 'center',
+        }}
+      >
+        <p className="text-[12px] font-inter font-semibold leading-4.5 text-white">
+          {payload[0].value.toLocaleString()} foydalanuvchi
+        </p>
+      </div>
+    );
+  }
+  return null;
 };
-export default CustomTooltip
+export default CustomTooltip;
