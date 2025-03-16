9e83999e23b6a54b9d9f5fdb0058c689d2aa61f7
Bakha022 <abduqahhorovbahodir536@gmail.com>
2025-03-16 09:03:38 +0500

    footer end

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import Contact from '@/containers/Contact';
 import Header from '@/containers/Header';
 import News from '@/containers/News';
@@ -6,13 +7,16 @@ import UsefulLinks from '@/containers/UsefulLinks';
 
 const HomePage = () => {
   return (
-    <div className="container">
-      <Header />
-      <News />
-      <Statstic />
-      <UsefulLinks />
-      <Contact />
-    </div>
+    <>
+      <div className="container">
+        <Header />
+        <News />
+        <Statstic />
+        <UsefulLinks />
+        <Contact />
+      </div>
+      <Footer />
+    </>
   );
 };
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,68 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-[#252525] pt-16 pb-12">
+      <div className="container">
+        <div className="flex flex-col gap-12 md:flex-row items-start md:items-center justify-between mb-16">
+          <Link href={'/'}>
+            <Image
+              width={112}
+              height={32}
+              alt="logo"
+              src={'/logoSecondary.svg'}
+            />
+          </Link>
+          <ul className="flex flex-col md:flex-row gap-8">
+            <li>
+              <Link
+                className="text-base font-inter font-semibold left-6 text-white"
+                href={'/'}
+              >
+                Yangiliklar
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-base font-inter font-semibold left-6 text-white"
+                href={'/'}
+              >
+                Statistika
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-base font-inter font-semibold left-6 text-white"
+                href={'/'}
+              >
+                Foydali havolalar
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-base font-inter font-semibold left-6 text-white"
+                href={'/'}
+              >
+                Kontakt
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-base font-inter font-semibold left-6 text-white"
+                href={'/'}
+              >
+                Qo’llanma
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <p className="pt-8 font-inter text-white font-normal text-base leading-6  border-t-[1px] border-[#F2F4F74D]">
+          © 2077 Untitled UI. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
