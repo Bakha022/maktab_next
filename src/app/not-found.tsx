4beb59a2e04c49541a06ff3a2524efcf97307233
Bakha022 <abduqahhorovbahodir536@gmail.com>
2025-03-21 13:08:56 +0500

    not found page added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const NotFound = () => {
-	return (
-		<div>NotFound</div>
-	)
-}
+  return (
+    <div className="container">
+      <div className="flex flex-col justify-center gap-5 items-center mt-[200px]">
+        <h1 className="text-center font-inter font-semibold text-4xl">
+          Bunday sahifa topilmadi
+        </h1>
+        <Link href={'/'}>
+          <Button className="font-inter font-semibold bg-blue-900 text-base leading-6">
+            Asosiy Sahifaga o'tish
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
